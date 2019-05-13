8d9913ffb5a10ae29d146d271a3a2ff5951ecce3
Eldin Anthony <eldin.anthony@gmail.com>
2019-05-13 09:52:14 +0200

    Mise a jour du titre !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'StockManagement';
+  title = 'Projet de fou pour changer la couleur des titres';
 }
